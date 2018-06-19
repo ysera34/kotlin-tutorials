f8a98919489ab5b4dfe21e75ff6941cbad574e6f
ysera34 <ycyoon34@gmail.com>
2018-06-20 00:08:30 +0900

    chapter04 RegisterUsers toolbar menu

--- black/chapter04/RegisterUsers/app/src/main/java/org/inframincer/registerusers/MainActivity.kt
+++ black/chapter04/RegisterUsers/app/src/main/java/org/inframincer/registerusers/MainActivity.kt
@@ -2,11 +2,39 @@ package org.inframincer.registerusers
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //set toolbar
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onPrepareOptionsMenu(menu: Menu?): Boolean {
+        return super.onPrepareOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.add_user->{
+                Toast.makeText(this, "add_user", Toast.LENGTH_SHORT).show()
+//                val intent: Intent = Intent(this.SaveUserActivity::class.java)
+//                startActivity(intent)
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
