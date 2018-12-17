59cecd33a685ac422a3c50e37f9e12cfa4ba28ab
ysera34 <ycyoon34@gmail.com>
2018-12-17 23:40:22 +0900

    chapter03 MyApplication button click listener

--- survival/chapter03/MyApplication/app/src/main/java/org/inframincer/myapplication/MainActivity.kt
+++ survival/chapter03/MyApplication/app/src/main/java/org/inframincer/myapplication/MainActivity.kt
@@ -2,11 +2,16 @@ package org.inframincer.myapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            textView.text = "button clicked"
+        }
     }
 }
