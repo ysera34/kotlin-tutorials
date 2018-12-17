99d5059add175f45d4d33d83360e325ad290cb55
ysera34 <ycyoon34@gmail.com>
2018-12-18 00:51:00 +0900

    chapter07 MyWebBrowser step02 web view setting

--- survival/chapter07/MyWebBrowser/app/src/main/java/org/inframincer/mywebbrowser/MainActivity.kt
+++ survival/chapter07/MyWebBrowser/app/src/main/java/org/inframincer/mywebbrowser/MainActivity.kt
@@ -2,11 +2,36 @@ package org.inframincer.mywebbrowser
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.EditorInfo
+import android.webkit.WebViewClient
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // webView default settings
+        webView.apply {
+            settings.javaScriptEnabled = true
+            webViewClient = WebViewClient()
+        }
+        webView.loadUrl("http://www.google.com")
+        urlEditText.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                webView.loadUrl(urlEditText.text.toString())
+                true
+            } else {
+                false
+            }
+        }
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
     }
 }
