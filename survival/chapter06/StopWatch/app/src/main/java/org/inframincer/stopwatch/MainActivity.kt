fcc485363fde0e913c3e0b19ef9fd9982f4e755a
ysera34 <ycyoon34@gmail.com>
2018-12-18 00:33:05 +0900

    chapter06 StopWatch step03 record lap time

--- survival/chapter06/StopWatch/app/src/main/java/org/inframincer/stopwatch/MainActivity.kt
+++ survival/chapter06/StopWatch/app/src/main/java/org/inframincer/stopwatch/MainActivity.kt
@@ -2,6 +2,7 @@ package org.inframincer.stopwatch
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 import kotlin.concurrent.timer
@@ -11,6 +12,7 @@ class MainActivity : AppCompatActivity() {
     private var time = 0
     private var isRunning = false
     private var timerTask: Timer? = null
+    private var lap = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +24,14 @@ class MainActivity : AppCompatActivity() {
             } else {
                 pause()
             }
+        }
+
+        resetFab.setOnClickListener {
+            reset()
+        }
+
+        lapButton.setOnClickListener {
+            recordLapTime()
         }
     }
     private fun start() {
@@ -39,5 +49,24 @@ class MainActivity : AppCompatActivity() {
     private fun pause() {
         playFab.setImageResource(R.drawable.ic_play_arrow_black_24dp)
         timerTask?.cancel()
+    }
+
+    private fun reset() {
+        timerTask?.cancel()
+        time = 0
+        isRunning = false
+        playFab.setImageResource(R.drawable.ic_play_arrow_black_24dp)
+        hmTextView.text = getString(R.string.init_second)
+        msTextView.text = getString(R.string.init_milli_second)
+        lapLayout.removeAllViews()
+        lap = 1
+    }
+    private fun recordLapTime() {
+        val lapTime = this.time
+        val textView = TextView(this)
+//        textView.text = "$lap LAB : ${lapTime / 100}.${lapTime % 100}"
+        textView.text = getString(R.string.lap_time_format, lap, lapTime / 100, lapTime % 100)
+        lapLayout.addView(textView, 0)
+        lap++
     }
 }
