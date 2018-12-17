2189572d9a9dc22295cf00bde4854d290155d82c
ysera34 <ycyoon34@gmail.com>
2018-12-18 00:28:30 +0900

    chapter06 StopWatch step02 timer

--- survival/chapter06/StopWatch/app/src/main/java/org/inframincer/stopwatch/MainActivity.kt
+++ survival/chapter06/StopWatch/app/src/main/java/org/inframincer/stopwatch/MainActivity.kt
@@ -2,11 +2,42 @@ package org.inframincer.stopwatch
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+import kotlin.concurrent.timer
 
 class MainActivity : AppCompatActivity() {
+
+    private var time = 0
+    private var isRunning = false
+    private var timerTask: Timer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        playFab.setOnClickListener {
+            isRunning = !isRunning
+            if (isRunning) {
+                start()
+            } else {
+                pause()
+            }
+        }
+    }
+    private fun start() {
+        playFab.setImageResource(R.drawable.ic_pause_black_24dp)
+        timerTask = timer(period = 10) {
+            time++
+            val sec = time / 100
+            val milli = time % 100
+            runOnUiThread {
+                hmTextView.text = "$sec"
+                msTextView.text = "$milli"
+            }
+        }
+    }
+    private fun pause() {
+        playFab.setImageResource(R.drawable.ic_play_arrow_black_24dp)
+        timerTask?.cancel()
     }
 }
