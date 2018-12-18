29fba4de35ae4643e36fe06118f45e6e38e30ec4
ysera34 <ycyoon34@gmail.com>
2018-12-19 01:07:12 +0900

    chapter08 TiltSensor step02 use sensor

--- survival/chapter08/TiltSensor/app/src/main/java/org/inframincer/tiltsensor/MainActivity.kt
+++ survival/chapter08/TiltSensor/app/src/main/java/org/inframincer/tiltsensor/MainActivity.kt
@@ -1,11 +1,28 @@
 package org.inframincer.tiltsensor
 
+import android.content.Context
+import android.content.pm.ActivityInfo
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.WindowManager
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SensorEventListener {
+
+    private val sensorManager by lazy {
+        getSystemService(Context.SENSOR_SERVICE) as SensorManager
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+//        Prevent screen from turning off
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+//        Pinning the screen to landscape mode
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
@@ -20,10 +37,14 @@ class MainActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
+        sensorManager.registerListener(this,
+                sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_NORMAL)
     }
 
     override fun onPause() {
         super.onPause()
+        sensorManager.unregisterListener(this)
     }
 
     override fun onStop() {
@@ -32,5 +53,19 @@ class MainActivity : AppCompatActivity() {
 
     override fun onDestroy() {
         super.onDestroy()
+    }
+
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+
+    }
+
+    override fun onSensorChanged(p0: SensorEvent?) {
+//        Called when the sensor value changes.
+//        SensorEvent.values[0] // The x-axis value. Up -10 to 0 when tilted up, 0 to 10 when tilted down
+//        SensorEvent.values[1] // The x-axis value. -10 ~ 0 when tilted to the left, 0 ~ 10 when tilted to the right
+//        SensorEvent.values[2] // The x-axis value. unused
+        p0?.let {
+            Log.d("MainActivity", "onSensorChanged: x: ${p0.values[0]}, y: ${p0.values[1]}, z: ${p0.values[]}")
+        }
     }
 }
