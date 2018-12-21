4710eed52ae1aa033619b99c4d03cd99eccc4ccb
ysera34 <ycyoon34@gmail.com>
2018-12-21 14:50:54 +0900

    chapter11 Flashlight step02 using camera features in activity

--- survival/chapter11/Flashlight/app/src/main/java/org/inframincer/flashlight/MainActivity.kt
+++ survival/chapter11/Flashlight/app/src/main/java/org/inframincer/flashlight/MainActivity.kt
@@ -2,11 +2,22 @@ package org.inframincer.flashlight
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val torch = Torch(this)
+
+        flashSwitch.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                torch.flashOn()
+            } else {
+                torch.flashOff()
+            }
+        }
     }
 }
