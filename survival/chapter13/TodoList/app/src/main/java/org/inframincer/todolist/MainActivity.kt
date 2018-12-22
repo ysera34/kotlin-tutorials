46e7826de192e82117088f1b57b9b08ca81696f6
ysera34 <ycyoon34@gmail.com>
2018-12-22 13:30:19 +0900

    chapter13 todoList step01 xml layout

--- survival/chapter13/TodoList/app/src/main/java/org/inframincer/todolist/MainActivity.kt
+++ survival/chapter13/TodoList/app/src/main/java/org/inframincer/todolist/MainActivity.kt
@@ -15,7 +15,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
+        addFab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
