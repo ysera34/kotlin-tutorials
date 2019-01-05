968864995ca72479c8cf7e7f0924952e49e8db18
ysera34 <ycyoon34@gmail.com>
2019-01-05 21:41:05 +0900

    chapter07 Class & Object: 06 property accessor backing field

--- core/chapter07/ObjectOriented/src/Accessor.kt
+++ core/chapter07/ObjectOriented/src/Accessor.kt
@@ -1,11 +1,11 @@
 fun main(args: Array<String>) {
     val f1 = MyFriend()
-    println("${f1.name} ${f1.tel} ${f1.type}")
+    println("${f1.name} ${f1.tel} ${f1.type} ${f1.nameLength}")
 
-    f1.name = "john"
+    f1.name = "johnjohn"
     f1.tel = "000"
     f1.type = 2
-    println("${f1.name} ${f1.tel} ${f1.type}")
+    println("${f1.name} ${f1.tel} ${f1.type} ${f1.nameLength}")
 }
 
 class MyFriend {
@@ -32,10 +32,15 @@ class MyFriend {
     var type: Int = 4
         get() {
             println("called get accessor of type")
-            return field
+//            return type // Exception in thread "main" java.lang.StackOverflowError
+            return field // have to use a backing field
         }
         set(value) {
             println("called set accessor of type")
             field = if (value < 4) value else 4
         }
+
+    val nameLength: Int
+        get() = name.length // Do not create a backing field.
+//    val nameLength: Int = name.length
 }
