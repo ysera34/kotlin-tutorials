5a3b746ed12f6181c25bb2da92385023b41bfb15
ysera34 <ycyoon34@gmail.com>
2019-01-16 21:21:19 +0900

    chapter08 Lambda: 06 lambda and closure

---
+++ core/chapter08/Lambda/src/03Closure.kt
@@ -0,0 +1,28 @@
+fun calculateTotal1(from: Int, to: Int): Int {
+    var total: Int = 0
+
+    val lambda = there@ { from: Int, to: Int ->
+        for (i in from..to) {
+            total += i
+        }
+        return@there
+    }
+
+    lambda(from, to)
+    return total
+}
+
+fun main(args: Array<String>) {
+    println("1..100 total=${calculateTotal1(1, 100)}")
+
+    val numbers = Array(5){ i -> (i * i) }
+    println(numbers.filter { it % 2 == 0 })
+
+    val list = listOf(1, 2, 3, 4, 5)
+    println(list.map { it + 10 })
+
+    val friends = listOf(FriendMine("jack", 30, "000"),
+            FriendMine("ally", 31, "111"))
+    println(friends.filter { it.age >= 30 }.map(FriendMine::name))
+    println(friends.filter { it.age >= 30 }.map { FriendMine::name })
+}
