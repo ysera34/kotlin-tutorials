68a27f053ba4c92dc4d0176ae6b501209ee733bc
ysera34 <ycyoon34@gmail.com>
2019-03-14 00:29:35 +0900

    chapter02 2.3, 2.4

---
+++ action/chapter02/src/2_4.kt
@@ -0,0 +1,15 @@
+/** JAVA
+public class Person {
+    private final String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+*/
+/** Kotlin */
+class Person(val name: String)
