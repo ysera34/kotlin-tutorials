bed0143dda21e8d346a15c63543240f50b5e8e7d
ysera34 <ycyoon34@gmail.com>
2019-03-15 23:54:26 +0900

    chapter02 2.8

---
+++ action/chapter02/src/2_8.kt
@@ -0,0 +1,13 @@
+package geometry.shape
+
+import java.util.*
+
+class Rectangle(val height: Int, val width: Int) {
+    val isSquare: Boolean
+        get() = height == width
+}
+
+fun createRandomRectangle(): Rectangle {
+    val random = Random()
+    return Rectangle(random.nextInt(), random.nextInt())
+}
