62478d8793cfc1c0b4d489953e605237c69f904c
ysera34 <ycyoon34@gmail.com>
2019-03-25 21:25:13 +0900

    chapter02 2.22, 2.23

---
+++ action/chapter02/src/2_22.kt
@@ -0,0 +1,16 @@
+fun fizzBuzz(i: Int) = when {
+    i % 15 == 0 -> "FizzBuzz "
+    i % 3 == 0 -> "Fizz "
+    i % 5 == 0 -> "Buzz "
+    else -> "$i "
+}
+
+fun main(args: Array<String>) {
+    for (i in 1..100) {
+        print(fizzBuzz(i))
+    }
+    println()
+    for (i in 100 downTo 1 step 2) {
+        print(fizzBuzz(i))
+    }
+}
