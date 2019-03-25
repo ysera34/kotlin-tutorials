202ee90dce32b20ad17ecf3c7834340adc470389
ysera34 <ycyoon34@gmail.com>
2019-03-26 00:05:38 +0900

    chapter03 Functions: 3.1 ~ 3.3

---
+++ action/chapter03/src/3_1.kt
@@ -0,0 +1,41 @@
+import java.lang.StringBuilder
+
+fun <T> joinToString1(
+    collection: Collection<T>,
+    separator: String,
+    prefix: String,
+    postfix: String
+): String {
+    val result = StringBuilder(prefix)
+    for ((index, element) in collection.withIndex()) {
+        if (index > 0) result.append(separator)
+        result.append(element)
+    }
+    result.append(postfix)
+    return result.toString()
+}
+
+fun <T> joinToString2(
+    collection: Collection<T>,
+    separator: String = ", ",
+    prefix: String = "",
+    postfix: String = ""
+): String {
+    val result = StringBuilder(prefix)
+    for ((index, element) in collection.withIndex()) {
+        if (index > 0) result.append(separator)
+        result.append(element)
+    }
+    result.append(postfix)
+    return result.toString()
+}
+
+fun main(args: Array<String>) {
+    val list = listOf(1, 2, 3)
+    println(joinToString1(list, "; ", "(", ")"))
+
+    println(joinToString2(list, ", ", "", ""))
+    println(joinToString2(list))
+    println(joinToString2(list, "; "))
+    println(joinToString2(list, postfix = ";", prefix = "# "))
+}
