856e0ef1487390762ca82636d481d209efb52392
ysera34 <ycyoon34@gmail.com>
2019-05-14 23:54:21 +0900

    spitz day01: module lib

--- spitz/day01/root/app/src/main/kotlin/main.kt
+++ spitz/day01/root/app/src/main/kotlin/main.kt
@@ -1,3 +1,4 @@
 fun main() {
     println("hello world in app")
+    hello()
 }
--- spitz/day01/root/app/web/app.js
+++ spitz/day01/root/app/web/app.js
@@ -1,14 +1,19 @@
 if (typeof kotlin === 'undefined') {
   throw new Error("Error loading module 'app'. Its dependency 'kotlin' was not found. Please, check whether 'kotlin' is loaded prior to 'app'.");
 }
-var app = function (_, Kotlin) {
+if (typeof lib === 'undefined') {
+  throw new Error("Error loading module 'app'. Its dependency 'lib' was not found. Please, check whether 'lib' is loaded prior to 'app'.");
+}
+var app = function (_, Kotlin, $module$lib) {
   'use strict';
   var println = Kotlin.kotlin.io.println_s8jyv4$;
+  var hello = $module$lib.hello;
   function main() {
     println('hello world in app');
+    hello();
   }
   _.main = main;
   main();
   Kotlin.defineModule('app', _);
   return _;
-}(typeof app === 'undefined' ? {} : app, kotlin);
+}(typeof app === 'undefined' ? {} : app, kotlin, lib);
